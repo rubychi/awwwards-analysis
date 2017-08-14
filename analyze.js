8522b8bb796d45c56c872999cb99556d8d6868ec
RubyChi <nothelse@hotmail.com>
2017-08-14 19:38:35 +0800

    Adjust tooltip on mobile

--- analyze.js
+++ analyze.js
@@ -101,11 +101,11 @@ function analyze(error, time, nominees, honorable, developer, sotd, sotm, soty) 
   var tipTriangleRight = {};
   if (isMobileOrTablet) {
     tipMargin.top = -65;
-    tipMargin.left = 10;
+    tipMargin.left = 11;
     tipTriangleLeft.bottom = "-33px";
     tipTriangleLeft.left = "-7px";
-    tipTriangleRight.bottom = "-30px";
-    tipTriangleRight.left = "174px";
+    tipTriangleRight.bottom = "-35px";
+    tipTriangleRight.left = "173px";
   } else {
     tipMargin.top = -60;
     tipMargin.left = 10;
