5e8858a94c133b816a9a6fd6c814512fc313330f
RubyChi <nothelse@hotmail.com>
2017-08-15 17:47:23 +0800

    Testing heroku

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 let app = express();
 let publicPath = path.join(__dirname, './docs');
-app.use(express.static('docs'));
+app.use(express.static(publicPath));
 
-app.listen(3000, () => console.log(`Server is up on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is up on port ${PORT}`));
