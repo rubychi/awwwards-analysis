239a7b6798308ef07ddb4bda3a8b409eea08b617
RubyChi <nothelse@hotmail.com>
2017-08-15 15:10:49 +0800

    Update crawler to execute requests in parallel

--- assets/scripts/crawler.js
+++ assets/scripts/crawler.js
@@ -1,6 +1,7 @@
+let retry = require('async-retry');
 let axios = require('axios');
 let cheerio = require('cheerio');
-let mkdirp = require('mkdirp');
+let mkdirp = require('mkdirp-promise');
 let fs = require('fs');
 let getDirName = require('path').dirname;
 
@@ -8,13 +9,73 @@ const AWWWARDS_PAGE = 'https://www.awwwards.com/websites/';
 const WORLD_POP_API = 'https://restcountries.eu/rest/v2/name/';
 const PATHS = ['nominees', 'honorable', 'sites_of_the_day', 'sites_of_the_month', 'sites_of_the_year', 'developer'];
 
-(async function() {
-  /* Create missing parent folder while writing file */
-  async function writeFile(path, contents, cb) {
-    let err = await mkdirp(getDirName(path));
-    if (err) return cb(err);
-    fs.writeFile(path, contents, cb);
-  }
+(async () => {
+  PATHS.forEach(async path => {
+    // Fetch AWWWARDS website
+    let res = null;
+    await retry(async () => {  res = await axios.get(`${AWWWARDS_PAGE}${path}`) } );
+    let result = {};
+    let $ = cheerio.load(res.data);
+    result[path] = [];
+    $('.name-filter').each((i, el) => {
+      if (el.children[0].data.toLowerCase() === 'country') {
+        let dropDownMenu = $(el).next();
+        // Raw html: <a href="/websites/Argentina/" data-count="46">Argentina</a>
+        dropDownMenu.find('a').each((i, el) => {
+          let key = $(el).text();
+          if (result[path].filter(x => x.country === key).length === 0) {
+            let data = {};
+            key = cnvCtryName(key);
+            // Exclude country which is not in the list of REST Countries API
+            if (key !== 'Cape Verde') {
+              data.country = key;
+              data.submission = $(el).data('count');
+              result[path].push(data);
+            }
+          }
+        });
+      }
+    });
+    // Handle exception: calculate the combined result for 'Hong Kong' and 'Macau'
+    await retry(async () => {
+      let data = result[path].filter(x => x.country === 'Hong Kong - Macau')[0];
+      if (data !== undefined) {
+        let res = await Promise.all([axios.get(`${WORLD_POP_API}Hong Kong`), axios.get(`${WORLD_POP_API}Macau`)]);
+        data.population = res[0].data[0].population + res[1].data[0].population;
+        data.percentage = ((data.submission / data.population) * 100);
+      }
+    });
+    // Fetch population for each country from REST Countries API
+    await Promise.all(result[path].map(async (item) => {
+      let url = `${WORLD_POP_API}${item.country}`;
+      // In order to get the correct data for 'India' (not 'British Indian Ocean Territory'),
+      // we need to set an additional parameter 'fullText' to be true
+      if (item.country === 'India') {
+        url = `${WORLD_POP_API}${item.country}?fullText=true`
+      }
+      if (item.country !== 'Hong Kong - Macau') {
+        let res = null;
+        await retry(async () => { res = await axios.get(url) });
+        data = result[path].filter(x => x.country === item.country)[0];
+        if (data.country === 'Korea (Republic of)') {
+          data.country = 'South Korea';
+        } else if (data.country === 'Korea (Democratic People\'s Republic of)') {
+          data.country = 'North Korea';
+        }
+        data.population = res.data[0].population;
+        data.percentage = ((data.submission / data.population) * 100);
+      }
+    }));
+    // Write result to csv files
+    let timestamp = `Timestamp\n${new Date()}\n`;
+    writeFile('./temp/data/timestamp.csv', timestamp);
+    let csv = 'Country,Percentage,Submission,Population\n';
+    let sortedResult = result[path].sort((a, b) => a.country > b.country ? 1 : -1);
+    sortedResult.forEach((item, i) => {
+      csv += `${item.country},${item.percentage},${item.submission},${item.population}\n`;
+    });
+    writeFile(`./temp/data/result (${path}).csv`, csv);
+  });
   /*
    * Convert the original country name to fit REST Countries API
    *
@@ -34,68 +95,10 @@ const PATHS = ['nominees', 'honorable', 'sites_of_the_day', 'sites_of_the_month'
     }
     return name;
   }
-  for (let path of PATHS) {
-    // Fetch raw AWWWARDS website
-    let res = await axios.get(`${AWWWARDS_PAGE}${path}`);
-    if (res.status === 200) {
-      let $ = cheerio.load(res.data);
-      let result = [];
-      $('.name-filter').each((i, el) => {
-        if (el.children[0].data.toLowerCase() === 'country') {
-          let dropDownMenu = $(el).next();
-          // Raw html: <a href="/websites/Argentina/" data-count="46">Argentina</a>
-          dropDownMenu.find('a').each((i, el) => {
-            let key = $(el).text();
-            if (result.filter(x => x.country === key).length === 0) {
-              let data = {};
-              key = cnvCtryName(key);
-              // Exclude country which is not in the list of REST Countries API
-              if (key !== 'Cape Verde') {
-                data.country = key;
-                data.submission = $(el).data('count');
-                result.push(data);
-              }
-            }
-          });
-        }
-      });
-      // Handle exception: calculate the combined result for 'Hong Kong' and 'Macau'
-      let data = result.filter(x => x.country === 'Hong Kong - Macau')[0];
-      if (data !== undefined) {
-        let resHK = await axios.get(`${WORLD_POP_API}Hong Kong`);
-        let resMC = await axios.get(`${WORLD_POP_API}Macau`);
-        data.population = resHK.data[0].population + resMC.data[0].population;
-        data.percentage = ((data.submission / data.population) * 100);
-      }
-      // Fetch population for each country from REST Countries API
-      await Promise.all(result.map(async (item) => {
-        let url = `${WORLD_POP_API}${item.country}`;
-        // In order to get the correct data for 'India' (not 'British Indian Ocean Territory'),
-        // we need to set an additional parameter 'fullText' to be true
-        if (item.country === 'India') {
-          url = `${WORLD_POP_API}${item.country}?fullText=true`
-        }
-        if (item.country !== 'Hong Kong - Macau') {
-          let res = await axios.get(url);
-          data = result.filter(x => x.country === item.country)[0];
-          if (data.country === 'Korea (Republic of)') {
-            data.country = 'South Korea';
-          } else if (data.country === 'Korea (Democratic People\'s Republic of)') {
-            data.country = 'North Korea';
-          }
-          data.population = res.data[0].population;
-          data.percentage = ((data.submission / data.population) * 100);
-        }
-      }));
-      // Write result to csv files
-      let timestamp = `Timestamp\n${new Date()}\n`;
-      writeFile('./temp/data/timestamp.csv', timestamp);
-      let csv = 'Country,Percentage,Submission,Population\n';
-      let sortedResult = result.sort((a, b) => a.country > b.country ? 1 : -1);
-      sortedResult.forEach((item, i) => {
-        csv += `${item.country},${item.percentage},${item.submission},${item.population}\n`;
-      });
-      writeFile(`./temp/data/result (${path}).csv`, csv);
-    }
+  /* Create missing parent folder while writing file */
+  async function writeFile(path, contents) {
+    let err = await mkdirp(getDirName(path));
+    if (err) throw err;
+    fs.writeFile(path, contents);
   }
 })()
