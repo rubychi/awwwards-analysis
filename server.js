3552745a7adb47c174a66451e116907a14cad82c
RubyChi <nothelse@hotmail.com>
2017-08-18 19:04:01 +0800

    Add helmet

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 let express = require('express');
+let helmet = require('helmet');
 let path = require('path');
 
 const PORT = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, './docs');
 
 let app = express();
-let publicPath = path.join(__dirname, './docs');
+app.use(helmet());
 app.use(express.static(publicPath));
 
 app.listen(PORT, () => console.log(`Server is up on port ${PORT}`));
