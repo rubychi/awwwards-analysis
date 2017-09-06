e6c3e44619c0ab36885d4e51812c6a7d3533d727
RubyChi <nothelse@hotmail.com>
2017-09-06 21:43:10 +0800

    Improve performance by gzip compression

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 let express = require('express');
-let expiry = require('static-expiry');
 let compression = require('compression');
 let helmet = require('helmet');
 let path = require('path');
@@ -8,7 +7,6 @@ const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, './docs');
 
 let app = express();
-app.use(expiry(app, {dir: publicPath}));
 app.use(compression());
 app.use(helmet());
 app.use(express.static(publicPath));
