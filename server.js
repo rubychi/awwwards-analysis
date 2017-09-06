ff86dbe1c054e4fbeab45b034004d011e52ed9da
RubyChi <nothelse@hotmail.com>
2017-09-06 21:32:24 +0800

    Improve performance by enable cache and compression

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 let express = require('express');
+let expiry = require('static-expiry');
+let compression = require('compression');
 let helmet = require('helmet');
 let path = require('path');
 
@@ -6,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, './docs');
 
 let app = express();
+app.use(expiry(app, {dir: publicPath}));
+app.use(compression());
 app.use(helmet());
 app.use(express.static(publicPath));
 
